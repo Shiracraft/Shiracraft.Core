a2bc0df67a0f450ac01eb6042448b20a7c834f15
theelicht <melleeluc@gmail.com>
2025-12-11 17:13:20 +0100

    Simplify item registration and register slot coin and pity token.

--- src/main/java/mc/shiracraft/core/client/ClientModEvents.java
+++ src/main/java/mc/shiracraft/core/client/ClientModEvents.java
@@ -1,6 +1,7 @@
 package mc.shiracraft.core.client;
 
 import com.mojang.logging.LogUtils;
+import mc.shiracraft.core.Core;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -8,10 +9,8 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import org.slf4j.Logger;
 
-import static mc.shiracraft.core.Core.MODID;
-
 // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-@Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+@Mod.EventBusSubscriber(modid = Core.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientModEvents
 {
     private static final Logger LOGGER = LogUtils.getLogger();
