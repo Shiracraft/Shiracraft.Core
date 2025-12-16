e9c7131d7b3489ed97910c303ca364b1b6fdd830
theelicht <melleeluc@gmail.com>
2025-12-16 11:20:47 +0100

    Add melon soda

---
+++ src/main/java/mc/shiracraft/core/items/MelonSoda.java
@@ -0,0 +1,25 @@
+package mc.shiracraft.core.items;
+
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.UseAnim;
+import org.jetbrains.annotations.NotNull;
+
+public class MelonSoda extends Item {
+    public static final String ITEM_NAME = "melon_soda";
+    private static final Properties PROPERTIES = new Properties()
+            .food(new FoodProperties.Builder()
+                    .nutrition(2)
+                    .saturationMod(0.2f)
+                    .build()
+            );
+    public MelonSoda() {
+        super(PROPERTIES);
+    }
+
+    @Override
+    public @NotNull UseAnim getUseAnimation(@NotNull ItemStack itemStack) {
+        return UseAnim.DRINK;
+    }
+}
