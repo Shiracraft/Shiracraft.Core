e4bdfb4e68626cafc2fdb137e57f8e9c63cb4037
theelicht <melleeluc@gmail.com>
2025-12-28 21:17:35 +0100

    Add unlock list command for debugging

--- src/main/java/mc/shiracraft/core/unlock/UnlockCategory.java
+++ src/main/java/mc/shiracraft/core/unlock/UnlockCategory.java
@@ -1,21 +1,28 @@
 package mc.shiracraft.core.unlock;
 
 import net.minecraft.util.StringRepresentable;
+import org.jetbrains.annotations.NotNull;
 
 public enum UnlockCategory implements StringRepresentable {
-    TECHNICAL_MOD("technical_mod"),
-    MAGICAL_MOD("magical_mod"),
-    FOOD_MOD("food_mod"),
-    OTHER("other");
+    TECHNICAL_MOD("technical_mod", "Technical Mods"),
+    MAGICAL_MOD("magical_mod", "Magical Mods"),
+    FOOD_MOD("food_mod", "Food Mods"),
+    OTHER("other", "Other");
 
     private final String name;
+    private final String displayName;
 
-    UnlockCategory(String name) {
+    UnlockCategory(String name, String displayName) {
         this.name = name;
+        this.displayName = displayName;
     }
 
     @Override
-    public String getSerializedName() {
+    public @NotNull String getSerializedName() {
         return name;
+    }
+
+    public String getDisplayName() {
+        return displayName;
     }
 }
